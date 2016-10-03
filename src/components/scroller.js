bb33a129ea4b3668e29bd8581fe1e1a7646bf66c
nigelgilbert <ngilbert@smu.edu>
2016-10-03 03:45:04 -0500

    initial ux experimenting

---
+++ src/components/scroller.js
@@ -0,0 +1,90 @@
+"use strict";
+
+// usage:
+// import { Scroller } from "./scroller";
+// Scroller.draw();
+//
+// requires an html node to bootstrap when drawing: 
+// <div class="scroller">
+
+// TODO: make your own debouncing service
+import debounce from "debounce";
+import TWEEN from "tween.js";
+import tweener from "../services/tweener.js";
+import "./scroller.css";
+
+
+
+function scrollTo(y) {
+    window.scrollTo(0,y);
+}
+
+export class Scroller {
+  constructor() {
+    this.tween = null;
+    this.onScroll = this.onScroll.bind(this);
+    this.onLoad = this.onLoad.bind(this);
+    this._nUnload = this.onUnload.bind(this);
+    this.animate = this.animate.bind(this);
+    this.cancel = this.cancel.bind(this);
+    this.draw = this.draw.bind(this);
+  }
+    
+  // initialize window event handlers
+  onLoad() {
+    console.log("load");
+    window.addEventListener("mousewheel", this.cancel);
+    window.addEventListener("scroll", debounce(this.onScroll, 250));
+  //  window.addEventListener("resize", this.handleResize);
+  }
+
+  // tear down window event handlers
+  onUnload() {
+    console.log("unload");
+  }
+
+  onScroll() {
+    const bounds = document.getElementById("top-buffer").getBoundingClientRect();
+    console.log(bounds);
+    if (bounds.bottom > 0) {
+      this.animate(bounds);
+    }
+  }
+
+  animate(bounds) {
+    this.animating = true;
+    const current = { y: window.pageYOffset }
+    const target = { y: window.pageYOffset + bounds.bottom };
+    const timespan = 500;
+
+    this.tween = tweener(current, target, 600)
+      .easing(TWEEN.Easing.Circular.Out)
+      .onUpdate(function() {
+        scrollTo(this.y);
+      })
+      .start();
+  }
+
+  cancel() {
+    if (this.tween !== null) {
+      this.tween.stop();
+      this.tween = null;
+    }
+  }
+
+  draw() {
+    window.onload = this.onLoad;
+    window.onbeforeunload = this.onUnload;
+    
+    document.addEventListener("DOMContentLoaded", () => {
+      document.getElementById("scroller").innerHTML  = `
+        <div id="inertia" class="inertial-container">
+          <div id="top-buffer" class="buffer"></div>
+          <div class="scroller"></div>
+          <div id="bottom-buffer" class="buffer"></div>
+        </div>
+      `;
+    });
+  }
+};
+
---
+++ src/index.js
@@ -0,0 +1,7 @@
+"use strict";
+
+import "./index.html";
+import { Scroller } from "./components/scroller.js";
+
+const scroller = new Scroller();
+scroller.draw();
