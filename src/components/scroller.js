545e7d822c27dc61fba193b2151b389e9cd9f633
= <ngilbert@smu.edu>
2016-12-08 09:32:15 -0600

    finished refactoring out es6 class keyword

--- src/components/scroller.js
+++ src/components/scroller.js
@@ -5,72 +5,93 @@ import TWEEN from "tween.js";
 import tweener from "../services/tweener.js";
 import "./scroller.css";
 
-function scrollTo(y) {
-    window.scrollTo(0,y);
+let tween = null;
+let isAnimating = false;
+let wheelHandler = null;
+let scrollHandler = null;
+
+export const Scroller = {
+  draw: draw
 }
 
-export class Scroller {
-  constructor() {
-    this.tween = null;
-    this.onScroll = this.onScroll.bind(this);
-    this.onLoad = this.onLoad.bind(this);
-    this.onUnload = this.onUnload.bind(this);
-    this.animate = this.animate.bind(this);
-    this.disableScrolling = this.disableScrolling.bind(this);
-    this.draw = this.draw.bind(this);
-  }
-    
-  // initialize window event handlers
-  onLoad() {
-    console.log("load");
-    window.addEventListener("mousewheel", debounce(this.onScroll, 750));
-    window.addEventListener("scroll", debounce(this.onScroll, 750));
-  //  window.addEventListener("resize", this.handleResize);
-  }
+function onLoad() {
+  enableScrolling()
+}
 
-  // tear down window event handlers
-  onUnload() {
-    console.log("unload");
+function onUnload() {}
+
+function enableScrolling() {
+  setTimeout(() => {
+    window.onscroll = debounce(onScroll, 400);
+    window.onmousewheel = debounce(onScroll, 400); 
+  }, 250);
+}
+
+function disableScrolling() {
+  window.onscroll = preventDefault;
+  window.onmousewheel = preventDefault;
+}
+
+function preventDefault(event) {
+  event = event || window.event;
+  if (event.preventDefault) {
+    event.preventDefault();
   }
+  event.returnValue = false;  
+}
 
-  onScroll() {
-    if (!this.animating) {
-      this.disableScrolling();
-      const topBuffer = document.getElementById("top-buffer").getBoundingClientRect();
-      if (topBuffer.bottom > 0) {
-        this.animate(topBuffer);
-      }
-    }
+function onScroll(event) {
+  const topBufferRect = document.getElementById("top-buffer").getBoundingClientRect();
+  if (!isAnimating && topBufferRect.bottom > 0) {
+    beginAnimation(topBufferRect);
   }
+}
 
-  animate(bounds) {
-    this.animating = true;
-    const current = { y: window.pageYOffset };
-    const target = { y: window.pageYOffset + bounds.bottom };
-    const timespan = 400;
-
-    this.tween = tweener(current, target, timespan)
-      .easing(TWEEN.Easing.Circular.Out)
-      .onUpdate(function() {
-        scrollTo(this.y);
-      })
-      .onComplete(() => this.animating = false)
-      .start();
+function checkAnimationCompletion() {
+  const topBufferRect = document.getElementById("top-buffer").getBoundingClientRect();
+  if (topBufferRect.bottom <= 0 && tween !== null) {
+    stopAnimation();
   }
+}
 
-  draw() {
-    window.onload = this.onLoad;
-    window.onbeforeunload = this.onUnload;
-    
-    document.addEventListener("DOMContentLoaded", () => {
-      document.getElementById("scroller").innerHTML  = `
-        <div id="inertia" class="inertial-container">
-          <div id="top-buffer" class="buffer"></div>
-          <div class="scroller"></div>
-          <div id="bottom-buffer" class="buffer"></div>
-        </div>
-      `;
-    });
-  }
-};
+function beginAnimation(bounds) {
+  isAnimating = true;
+  disableScrolling();
+  
+  const current = { y: window.pageYOffset };
+  const target = { y: window.pageYOffset + bounds.bottom };
+  const timespan = 400;
 
+  tween = tweener(current, target, timespan)
+    .easing(TWEEN.Easing.Circular.Out)
+    .onUpdate(function() {
+      window.scrollTo(0, this.y);
+      checkAnimationCompletion();
+    })
+    .onComplete(() => isAnimating = false)
+    .start();
+}
+
+function stopAnimation() {
+  tween.stop();
+  tween = null;
+  isAnimating = false;
+  enableScrolling();
+}
+
+function draw(target) {
+  target = target || 'scroller';
+
+  window.onload = onLoad;
+  window.onbeforeunload = onUnload;
+
+  document.addEventListener('DOMContentLoaded', () => {
+    document.getElementById(target).innerHTML  = `
+      <div id="inertia" class="inertial-container">
+        <div id="top-buffer" class="buffer"></div>
+        <div class="scroller"></div>
+        <div id="bottom-buffer" class="buffer"></div>
+      </div>
+    `;
+  });
+}
